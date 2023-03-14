1a9c857310e4454cac45ac5e9fed39ec1026d317
Miguel Souto <miguelsouto@192.168.1.14>
2023-03-14 20:50:40 -0300

    File structure to create suites

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,12 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
     },
-    specPattern: 'cypress/e2e/*.feature',
+    specPattern: 'cypress/e2e/suites/**/*.feature',
     baseUrl: 'https://www.saucedemo.com/'
   },
 });
